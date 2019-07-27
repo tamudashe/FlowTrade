145b771f9985b7c215b2c993d4f95ec9516a6b25
tamudashe <tamudashe@gmail.com>
2019-07-27 19:46:13 -0400

    final

--- tradingAssistant.py
+++ tradingAssistant.py
@@ -76,11 +76,8 @@ def machineL(ticker):
     X, y, df = get_fSets(ticker)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
 
-    # clf = neighbors.KNeighborsClassifier()
-    clf = VotingClassifier([("lsvc", svm.LinearSVC()),
-                            ("knn", neighbors.KNeighborsClassifier()),
-                            ("rfor", RandomForestClassifier())
-                            ])
+    clf = neighbors.KNeighborsClassifier()
+
 
     clf.fit(X_train, y_train)
     confidence = clf.score(X_test, y_test)
